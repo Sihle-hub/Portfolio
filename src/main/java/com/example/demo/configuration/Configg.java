490dc4c9d605ad515e87a52a00e5da563915d800
sihle <sihlesithole810@gmail.com>
2024-06-28 20:13:15 +0200

    porfolio datasource

---
+++ src/main/java/com/example/demo/configuration/Configg.java
@@ -0,0 +1,26 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Configg implements WebMvcConfigurer{
+
+	 @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/static/**")
+	                .addResourceLocations("classpath:/static/")
+	                .setCachePeriod(3600)
+	                .resourceChain(true);
+	    }
+
+	    @Override
+	    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	        configurer.defaultContentType(MediaType.APPLICATION_OCTET_STREAM);
+	    }
+}
+
+
